aad3a07a2e4f20b691de5a2b43b156aab5413030
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2023-02-13 19:41:19 +0100

    Nuevo archivo

---
+++ habbo_id.py
@@ -0,0 +1,19 @@
+import requests
+import pyperclip as clipboard
+
+
+
+habbo_id = input("Escribe el nombre del keko: ")
+
+habbo_keko = requests.get(f"https://www.habbo.es/api/public/users?name={habbo_id}")
+
+
+id = habbo_keko.json()['uniqueId']
+
+
+clipboard.copy(id)
+
+clipboard.paste()
+
+
+print("ID Copiado al portapepeles")
